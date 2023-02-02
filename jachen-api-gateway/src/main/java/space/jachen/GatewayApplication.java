fda7e3d0c2269224701fd344f1636995326776bf
Jachen <914158173@qq.com>
2023-02-02 23:30:00 +0800

    添加服务网关
    Signed-off-by: Jachen <914158173@qq.com>

---
+++ jachen-api-gateway/src/main/java/space/jachen/GatewayApplication.java
@@ -0,0 +1,17 @@
+package space.jachen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ${end}
+ *
+ * @author JaChen
+ * @date ${DATE} ${TIME}
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+    }
+}
